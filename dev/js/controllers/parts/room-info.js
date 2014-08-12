f918959db1fa8c44382eb030b39e25cf64b1564b
Rodion Mescheryakov <r.mescheryakov@fpmsoft.com>
2014-08-12 16:32:59 +0300

    RoomInfo использует сервисы

--- dev/js/controllers/parts/room-info.js
+++ dev/js/controllers/parts/room-info.js
@@ -1,10 +1,45 @@
-/* globals BauVoiceApp, STEP, typingTextWithDelay */
+/* globals STEP, typingTextWithDelay */
 
 'use strict';
 
-BauVoiceApp.controller('RoomInfoCtrl', ['$scope', function ($scope) {
+BauVoiceApp.controller('RoomInfoCtrl', ['$scope', 'locationService', 'constructService', function ($scope, locationService, constructService) {
   var $coeffTitle = $('.coeff-container .coeff-title'),
       DELAY_SHOW_COEFF_TITLE = 20 * STEP;
 
   typingTextWithDelay($coeffTitle, DELAY_SHOW_COEFF_TITLE);
+
+  $scope.roomInfo = {};
+
+  locationService.getCity(function (results) {
+    if (results.status) {
+      $scope.roomInfo.zone = results.data.zone.id;
+    } else {
+      console.log(results);
+    }
+  });
+
+  constructService.getRoomInfo(function (results) {
+    if (results.status) {
+      $scope.roomInfo.room = results.data.name;
+    } else {
+      console.log(results);
+    }
+  });
+
+  constructService.getCoefs(function (results) {
+    if (results.status) {
+      $scope.roomInfo.coefs = {
+        thermalResistance: {
+          required: results.data.coefs.thermalResistance.required,
+            actual: results.data.coefs.thermalResistance.actual
+        },
+        airCirculation: {
+          required: results.data.coefs.airCirculation.required,
+            actual: results.data.coefs.airCirculation.actual
+        }
+      };
+    } else {
+      console.log(results);
+    }
+  });
 }]);
