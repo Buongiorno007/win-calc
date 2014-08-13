aaf9942a7929fceb69dc8d1e521fea9df01048c8
Andrey Protasov <a.protasov@fpmsoft.com>
2014-08-13 17:49:55 +0300

    Elements List - верстка

---
+++ dev/js/controllers/menus/elements-list.js
@@ -0,0 +1,8 @@
+/* globals BauVoiceApp, STEP, typingIndex, typingTextByChar, createPrice, showElementWithDelay, removeClassWithDelay, addClassWithDelay */
+
+'use strict';
+
+BauVoiceApp.controller('ElementsListCtrl', ['$scope', function ($scope) {
+
+
+}]);
