1df818124a758542ae386b2cdb2a8d09c124227f
amulek <amulek@mail.ru>
2014-08-15 12:14:22 +0300

    Add new Service and add example of use first Import Database

--- dev/js/services/constructService.js
+++ dev/js/services/constructService.js
@@ -109,15 +109,12 @@ BauVoiceApp.factory('constructService', function () {
         note: 'Срочный заказ'
       }));
     },
-    getAllLaminations: function (factoryId, callback) {
-      var selectLaminations = "SELECT id, name FROM lamination_colors WHERE factory_id = " + factoryId,
-          db = openDatabase('shopping-list', '1.0', 'shopping-list', 65536),
-          i;
-
+    getAllLaminations: function (callback) {
+      var selectLaminations = "SELECT id, name FROM lamination_colors ORDER BY id",
+      db = openDatabase('bauvoice', '1.0', 'bauvoice', 65536), i;
       db.transaction(function (transaction) {
         transaction.executeSql(selectLaminations, [], function (transaction, result) {
           var allLaminations = [];
-
           if (result.rows.length) {
             for (i = 0; i < result.rows.length; i++) {
               allLaminations.push({
@@ -131,6 +128,44 @@ BauVoiceApp.factory('constructService', function () {
           }
         }, function () {
           callback(new ErrorResult(2, 'Something went wrong with selection lamination_colors record'));
+        });
+      });
+    },
+    getAllProfileSystems: function (callback) {
+      var db = openDatabase('bauvoice', '1.0', 'bauvoice', 65536);
+      db.transaction(function (transaction) {
+        transaction.executeSql("SELECT id, name FROM profile_system_folders order by position", [], function (transaction, result) {
+          if (result.rows.length) {
+            var AllProfileSystems = [];
+            var allFolders = result.rows.length - 1;
+            db.transaction(function (transaction) {
+              for (var j = 0; j < result.rows.length; j++) {
+                var count = 0;
+                var folder_id = result.rows.item(j).id;
+                transaction.executeSql("SELECT profile_systems.id, profile_system_folders.name as folder_name, profile_systems.name, profile_systems.short_name, profile_systems.country FROM profile_systems LEFT JOIN profile_system_folders ON  profile_systems.profile_system_folder_id = profile_system_folders.id WHERE profile_system_folder_id = ? order by profile_systems.position", [folder_id], function (transaction, result) {
+                  if (result.rows.length) {
+                    var resultObj = {folder: result.rows.item(0).folder_name, profiles: []};
+                    for (var i = 0; i < result.rows.length; i++) {
+                      resultObj.profiles.push({id: result.rows.item(i).id, name: "" + result.rows.item(i).name + "", short_name: "" + result.rows.item(i).short_name + "", country: "" + result.rows.item(i).country + ""});
+                    }
+                    AllProfileSystems.push(resultObj);
+                    if (allFolders === count) {
+                      callback(new OkResult(AllProfileSystems));
+                    }
+                    count++;
+                  } else {
+                    callback(new ErrorResult(1, 'No ProfileSystems in database!'));
+                  }
+                }, function () {
+                  callback(new ErrorResult(2, 'Something went wrong with selection profile_systems record'));
+                });
+              }
+            });
+          } else {
+            callback(new ErrorResult(1, 'No ProfileSystemFolders in database!'));
+          }
+        }, function () {
+          callback(new ErrorResult(2, 'Something went wrong with selection profile_system_folders record'));
         });
       });
     }
