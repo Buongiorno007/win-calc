f4966dc7f875790a55f3fc92cb4c77f70c27b63a
Rodion Mescheryakov <r.mescheryakov@fpmsoft.com>
2014-08-15 15:41:18 +0300

    Добавлен контейнер для вывода кода доступа на экране Login

--- dev/js/controllers/login.js
+++ dev/js/controllers/login.js
@@ -2,10 +2,24 @@
 
 'use strict';
 
-BauVoiceApp.controller('LoginCtrl', ['$scope', '$location', function ($scope, $location, $http) {
+BauVoiceApp.controller('LoginCtrl', ['$scope', '$location', '$translate', function ($scope, $location, $translate) {
+  $scope.isCodeValid = false;
+  $scope.isCodeCheckingSuccess = true;
+
+  $scope.checkCode = function () {
+    // запрос на сервер с проверкой кода. Если проверка прошла успешно, то синхронизировать базу данных.
+    // Если неуспешно, то выдать сообщение об ошибке.
+    $scope.isCodeCheckingSuccess = !$scope.isCodeCheckingSuccess;
+
+    if ($scope.isCodeCheckingSuccess) {
+      $scope.isCodeValid = true;
+    }
+  };
+
   // Вряд ли нужна такая проверка, т.к. пользователю будет неудобно вводить номер со скобками и дефисами.
   // К тому же форматы номеров отличаются в разных странах.
 //  $scope.regPhone = /^\+\d{2}\(\d{3}\)\d{3}-\d{4}$/;
+
   $scope.user = {};
 
   $scope.submitForm = function (form) {
@@ -14,16 +28,6 @@ BauVoiceApp.controller('LoginCtrl', ['$scope', '$location', function ($scope, $l
 
     if (form.$valid) {
       $location.path('/main');
-
-      //      $http.post('users.php', $scope.user)
-      //        .success(function (respond) {
-      //          console.log(respond);
-      //          if (respond.success) {
-      //            alert('Добро пожаловать');
-      //          } else {
-      //            alert('Нет такого пользователя');
-      //          }
-      //        });
     }
   };
 }]);
