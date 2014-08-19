b7894032034ec1a2e99de3ea17dd2a85e0c8d523
amulek <amulek@mail.ru>
2014-08-19 11:34:42 +0300

    upgrade code of services

--- dev/js/services/constructService.js
+++ dev/js/services/constructService.js
@@ -1,7 +1,14 @@
 "use strict";
 
 BauVoiceApp.factory('constructService', function () {
+
+  // SQL requests for select data from tables
+  var selectLaminations = "SELECT id, name FROM lamination_colors ORDER BY id",
+    selectProfileSystemFolders = "SELECT id, name FROM profile_system_folders order by position",
+    selectProfileSystems = "SELECT profile_systems.id, profile_system_folders.name as folder_name, profile_systems.name, profile_systems.short_name, profile_systems.country FROM profile_systems LEFT JOIN profile_system_folders ON  profile_systems.profile_system_folder_id = profile_system_folders.id WHERE profile_system_folder_id = ? order by profile_systems.position";
+
   return {
+
     // TODO: Сервис готов
     getCoefs: function (callback) {
       callback(new OkResult({
@@ -17,6 +24,7 @@ BauVoiceApp.factory('constructService', function () {
         }
       }));
     },
+
     // TODO: Сервис готов
     getRoomInfo: function (callback) {
       callback(new OkResult({
@@ -24,12 +32,14 @@ BauVoiceApp.factory('constructService', function () {
         name: 'Детская'
       }));
     },
+
     // TODO: Сервис готов
     getConstructThumb: function (callback) {
       callback(new OkResult({
         url: '../img/config-menu/configMenu_itemIMG.png'
       }));
     },
+
     // TODO: Сервис готов
     getConstructSize: function (callback) {
       callback(new OkResult({
@@ -37,6 +47,7 @@ BauVoiceApp.factory('constructService', function () {
         height: 1250
       }));
     },
+
     // TODO: Сервис готов
     getProfileSystem: function (callback) {
       callback(new OkResult({
@@ -44,6 +55,7 @@ BauVoiceApp.factory('constructService', function () {
         name: 'Окошко S58'
       }));
     },
+
     // TODO: Сервис готов
     getGlass: function (callback) {
       callback(new OkResult({
@@ -51,6 +63,7 @@ BauVoiceApp.factory('constructService', function () {
         name: '4/16/4'
       }));
     },
+
     // TODO: Сервис готов
     getWindowHardware: function (callback) {
       callback(new OkResult({
@@ -58,6 +71,7 @@ BauVoiceApp.factory('constructService', function () {
         name: 'Немецкая'
       }));
     },
+
     // TODO: Сервис готов
     getLamination: function (callback) {
       callback(new OkResult({
@@ -71,6 +85,7 @@ BauVoiceApp.factory('constructService', function () {
         }
       }));
     },
+
     // TODO: Сервис готов
     getAdditionalElements: function (callback) {
       callback(new OkResult({
@@ -94,6 +109,7 @@ BauVoiceApp.factory('constructService', function () {
         ]
       }));
     },
+
     // TODO: Сервис готов
     getPrice: function (callback) {
       callback(new OkResult({
@@ -104,17 +120,18 @@ BauVoiceApp.factory('constructService', function () {
         }
       }));
     },
+
+    // TODO: Сервис готов
     getConstructNoteText: function (callback) {
       callback(new OkResult({
         note: 'Срочный заказ'
       }));
     },
+
     getAllLaminations: function (callback) {
-      var selectLaminations = "SELECT id, name FROM lamination_colors ORDER BY id",
-      db = openDatabase('bauvoice', '1.0', 'bauvoice', 65536), i;
+      var db = openDatabase('bauvoice', '1.0', 'bauvoice', 65536), i, allLaminations = [];
       db.transaction(function (transaction) {
         transaction.executeSql(selectLaminations, [], function (transaction, result) {
-          var allLaminations = [];
           if (result.rows.length) {
             for (i = 0; i < result.rows.length; i++) {
               allLaminations.push({
@@ -131,21 +148,21 @@ BauVoiceApp.factory('constructService', function () {
         });
       });
     },
+
     getAllProfileSystems: function (callback) {
-      var db = openDatabase('bauvoice', '1.0', 'bauvoice', 65536);
+      var db = openDatabase('bauvoice', '1.0', 'bauvoice', 65536), AllProfileSystems = [], allFolders, count, folder_id, resultObj = {}, j, i;
       db.transaction(function (transaction) {
-        transaction.executeSql("SELECT id, name FROM profile_system_folders order by position", [], function (transaction, result) {
+        transaction.executeSql(selectProfileSystemFolders, [], function (transaction, result) {
           if (result.rows.length) {
-            var AllProfileSystems = [];
-            var allFolders = result.rows.length - 1;
+            allFolders = result.rows.length - 1;
             db.transaction(function (transaction) {
-              for (var j = 0; j < result.rows.length; j++) {
-                var count = 0;
-                var folder_id = result.rows.item(j).id;
-                transaction.executeSql("SELECT profile_systems.id, profile_system_folders.name as folder_name, profile_systems.name, profile_systems.short_name, profile_systems.country FROM profile_systems LEFT JOIN profile_system_folders ON  profile_systems.profile_system_folder_id = profile_system_folders.id WHERE profile_system_folder_id = ? order by profile_systems.position", [folder_id], function (transaction, result) {
+              for (j = 0; j < result.rows.length; j++) {
+                count = 0;
+                folder_id = result.rows.item(j).id;
+                transaction.executeSql(selectProfileSystems, [folder_id], function (transaction, result) {
                   if (result.rows.length) {
-                    var resultObj = {folder: result.rows.item(0).folder_name, profiles: []};
-                    for (var i = 0; i < result.rows.length; i++) {
+                    resultObj = {folder: result.rows.item(0).folder_name, profiles: []};
+                    for (i = 0; i < result.rows.length; i++) {
                       resultObj.profiles.push({id: result.rows.item(i).id, name: "" + result.rows.item(i).name + "", short_name: "" + result.rows.item(i).short_name + "", country: "" + result.rows.item(i).country + ""});
                     }
                     AllProfileSystems.push(resultObj);
@@ -169,5 +186,6 @@ BauVoiceApp.factory('constructService', function () {
         });
       });
     }
+
   }
 });
