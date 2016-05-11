9555c3bc173e540ab49d107c324f87ef2c7fb513
alexandr <solo@splinestudio.com>
2016-05-11 14:29:26 +0300

    header

--- www/js/main.js
+++ www/js/main.js
@@ -1389,6 +1389,7 @@ var isDevice = ( /(Android|webOS|iPhone|iPad|iPod|BlackBerry|Windows Phone)/i.te
     thisCtrl.isStartImport = 0;
     thisCtrl.user = {};
     thisCtrl.factories = 0;
+    GlobalStor.global.loader = 0; 
 
     //------- translate
     thisCtrl.OFFLINE = $filter('translate')('login.OFFLINE');
@@ -1811,26 +1812,41 @@ var isDevice = ( /(Android|webOS|iPhone|iPad|iPod|BlackBerry|Windows Phone)/i.te
 
     /** =========== SIGN IN ======== */
     function loader() {
-      $timeout(function() { GlobalStor.global.isLoader2 = 25 }, 100)
-      $timeout(function() { GlobalStor.global.isLoader2 = 40 }, 1500)      
-      $timeout(function() { GlobalStor.global.isLoader2 = 65 }, 3000)
-      $timeout(function() { GlobalStor.global.isLoader2 = 90 }, 4000)
-      $timeout(function() { GlobalStor.global.isLoader2 = 94 }, 7000)
-      $timeout(function() { GlobalStor.global.isLoader2 = 95 }, 9000)
-      $timeout(function() { GlobalStor.global.isLoader2 = 96 }, 11000)
-      $timeout(function() { GlobalStor.global.isLoader2 = 97 }, 15000)
-      $timeout(function() { GlobalStor.global.isLoader2 = 98 }, 21000)
-      $timeout(function() { GlobalStor.global.isLoader2 = 99 }, 30000)
+      if (GlobalStor.global.isLoader3 === 1) {
+        if (GlobalStor.global.isLoader === 1) {
+          GlobalStor.global.isLoader3 = 0
+        }
+      }
+      if (GlobalStor.global.isLoader3 === 0) {
+        $timeout(function() { GlobalStor.global.isLoader3 = 1 }, 1) 
+        $timeout(function() { GlobalStor.global.isLoader2 = 0 }, 1)  
+        $timeout(function() { GlobalStor.global.isLoader2 = 25 }, 100)
+        $timeout(function() { GlobalStor.global.isLoader2 = 40 }, 1500)      
+        $timeout(function() { GlobalStor.global.isLoader2 = 65 }, 3000)
+        $timeout(function() { GlobalStor.global.isLoader2 = 90 }, 4000)
+        $timeout(function() { GlobalStor.global.isLoader2 = 94 }, 7000)
+        $timeout(function() { GlobalStor.global.isLoader2 = 95 }, 9000)
+        $timeout(function() { GlobalStor.global.isLoader2 = 96 }, 11000)
+        $timeout(function() { GlobalStor.global.isLoader2 = 97 }, 15000)
+        $timeout(function() { GlobalStor.global.isLoader2 = 98 }, 21000)
+        $timeout(function() { GlobalStor.global.isLoader2 = 99 }, 30000)
+        $timeout(function() { GlobalStor.global.isLoader3 = 0 }, 31000) 
+      }
     }
 
+    if (window.location.hash.length > 10) {
+      loader()
+    }
+
+
     function enterForm(form) {
-      loader();
       var newUserPassword;
 //      console.log('@@@@@@@@@@@@=', typethisCtrl.user.phone, thisCtrl.user.password);
       //------ Trigger validation flag.
       thisCtrl.submitted = 1;
       if (form.$valid) {
         GlobalStor.global.isLoader = 1;
+        loader();
         //------ check Internet
         //TODO thisCtrl.isOnline = $cordovaNetwork.isOnline();
         if(thisCtrl.isOnline) {
@@ -2083,13 +2099,17 @@ var isDevice = ( /(Android|webOS|iPhone|iPad|iPod|BlackBerry|Windows Phone)/i.te
     }
 
   function gotoSettingsPage() {
-    if(GlobalStor.global.gotoSettingsPage === 0) {
-      $timeout(function() {
-        $location.path('/change-lang');
-      }, 1);
-      $timeout(function() {
-        $location.path('/');
-      }, 1);
+    if (window.location.hash.length < 10) {
+      if(GlobalStor.global.gotoSettingsPage === 0) {
+        $timeout(function() {
+          $location.path('/change-lang');
+        }, 1);
+        $timeout(function() {
+          $location.path('/');
+        }, 1);
+        GlobalStor.global.gotoSettingsPage = 1;
+      }
+    } else {
       GlobalStor.global.gotoSettingsPage = 1;
     }
   }
@@ -9227,12 +9247,12 @@ function ErrorResult(code, message) {
   angular
     .module('BauVoiceApp')
     .constant('globalConstants', {
-      serverIP: 'http://api.windowscalculator.net',
-      printIP: 'http://windowscalculator.net:3002/orders/get-order-pdf/',
-      localPath: '/calculator/local/',
-      //serverIP: 'http://api.steko.com.ua',
-      //printIP: 'http://admin.steko.com.ua:3002/orders/get-order-pdf/',
-      //localPath: '/local/', //TODO ipad
+      // serverIP: 'http://api.windowscalculator.net',
+      // printIP: 'http://windowscalculator.net:3002/orders/get-order-pdf/',
+      // localPath: '/calculator/local/',
+      serverIP: 'http://api.steko.com.ua',
+      printIP: 'http://admin.steko.com.ua:3002/orders/get-order-pdf/',
+      localPath: '/local/', //TODO ipad
       STEP: 50,
       REG_LOGIN: /^[a-zA-Z?0-9?_?.?@?\-?]+$/,
       REG_PHONE: /^\d+$/, // /^[0-9]{1,10}$/
@@ -25288,6 +25308,10 @@ if(GlobalStor.global.glassesAll[g].glassLists[l].parent_element_id === GlobalSto
         loader: 0,
         isLoader: 0,
         isLoader2: 0,
+        isLoader3: 0,
+        gotoSettingsPage: 0,
+        startProgramm: 1, // for START
+        //------ navig
         gotoSettingsPage: 0,
         startProgramm: 1, // for START
         //------ navigation
