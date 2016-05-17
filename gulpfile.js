d1d33f81227e90e5074d934665a823ffac334036
alexandr <solo@splinestudio.com>
2016-05-17 18:46:36 +0300

    edit stylstyle for editOrder

--- gulpfile.js
+++ gulpfile.js
@@ -288,8 +288,8 @@ gulp.task('prod', function() {
 
 /**========= Загрузка на удаленный сервер =========*/
 
- // var server = config.server;
- var server = config.serverSteko;
+ var server = config.server;
+ // var server = config.serverSteko;
 
 
 /** upload index */
