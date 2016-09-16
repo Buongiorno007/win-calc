536902358cb473c11094f903f5841c1c0293cce1
s-voloshynenko <s.voloshynenko@gmail.com>
2016-09-16 16:55:31 +0300

    ..

--- www/js/main.js
+++ www/js/main.js
@@ -9359,9 +9359,7 @@ function ErrorResult(code, message) {
         var products = OrderStor.order.products;
         if(products.length > 0 ) {
           for(var x=0; x<products.length; x+=1) {
-            if(products[x].product_id > productIndex+1) {
-              OrderStor.order.products[x].product_id = products[x].product_id-1;
-            }
+            products[x].product_id = x+1;
           }
           //--------- Change order price
         } else {
