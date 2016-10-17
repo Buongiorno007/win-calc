97dde797e975b61ae2b769f627771b713a119391
s-voloshynenko <s.voloshynenko@gmail.com>
2016-10-17 16:27:15 +0300

    ..

--- gulpfile.js
+++ gulpfile.js
@@ -290,8 +290,8 @@ gulp.task('prod', function() {
 /**========= Загрузка на удаленный сервер =========*/
 
 
-// var server = config.server; 
-var server = config.serverSteko;
+var server = config.server; 
+//var server = config.serverSteko;
 
 /** upload index */
 gulp.task('upload-index', function () {
