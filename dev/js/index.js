0921878584f51c5b8035885839e19008722c331e
anton <a.shevelev@fpmsoft.com>
2017-01-04 10:35:34 +0200

    for upload
    Former-commit-id: 8f57014d9ff194ed259969c3c3d00f1741405a31

--- dev/js/index.js
+++ dev/js/index.js
@@ -9,34 +9,34 @@ var isDevice = (/(Android|webOS|iPhone|iPad|iPod|BlackBerry|Windows Phone)/i.tes
   if (/(chrome|Chromium|safari|firefox|Opera|Yandex|internet explorer|Seamonkey)/i.test(window.navigator.userAgent)) {
     isDevice = 0;
   }
-  window.onload = function () {
-    if(!isDevice){location.hash = "#/";}
-    var elm = document.getElementById('main-frame'); // all -- элемент, в который был обернут весь сайт
-    var coeff = document.documentElement.clientHeight / elm.offsetHeight; // считаем коэффициент масштабирования так, чтобы элемент all занял весь экран
-    if (coeff > 1) coeff = 1; // нам нужно только уменьшение сайта, но не его увеличение, поэтому ограничиваем коэффициент сверху единицей
-    if (coeff < 0.6) coeff = 0.6; // ограничение снизу добавлено для того, чтобы сайт совсем уж не превращался в нечитаемый
-    if (coeff != 1.0) {
-      if (navigator.userAgent.indexOf('Firefox') != -1) elm.style.boxShadow = 'none';  // масштабирование в Firefox порождало некорректное отображение boxshadow, и пришлось это свойство отключить
-      elm.style.webkitTransform = 'scale(' + coeff + ')';
-      elm.style.msTransform =
-        elm.style.mozTransform =
-          elm.style.transform = 'scale(' + coeff + ')'; // собственно масштабирование
-    }
-  };
-  window.onresize = function (event) {
-    var elm = document.getElementById('main-frame'); // all -- элемент, в который был обернут весь сайт
-    var coeff = document.documentElement.clientHeight / elm.offsetHeight; // считаем коэффициент масштабирования так, чтобы элемент all занял весь экран
-    if (coeff > 1) coeff = 1; // нам нужно только уменьшение сайта, но не его увеличение, поэтому ограничиваем коэффициент сверху единицей
-    if (coeff < 0.6) coeff = 0.6; // ограничение снизу добавлено для того, чтобы сайт совсем уж не превращался в нечитаемый
-    if (coeff != 1.0) {
-      if (navigator.userAgent.indexOf('Firefox') != -1) elm.style.boxShadow = 'none';  // масштабирование в Firefox порождало некорректное отображение boxshadow, и пришлось это свойство отключить
-      elm.style.webkitTransform = 'scale(' + coeff + ')';
-      elm.style.msTransform =
-        elm.style.mozTransform =
-          elm.style.transform = 'scale(' + coeff + ')'; // собственно масштабирование
-    }
-
-  };
+  // window.onload = function () {
+  //   if(!isDevice){location.hash = "#/";}
+  //   var elm = document.getElementById('main-frame'); // all -- элемент, в который был обернут весь сайт
+  //   var coeff = document.documentElement.clientHeight / elm.offsetHeight; // считаем коэффициент масштабирования так, чтобы элемент all занял весь экран
+  //   if (coeff > 1) coeff = 1; // нам нужно только уменьшение сайта, но не его увеличение, поэтому ограничиваем коэффициент сверху единицей
+  //   if (coeff < 0.6) coeff = 0.6; // ограничение снизу добавлено для того, чтобы сайт совсем уж не превращался в нечитаемый
+  //   if (coeff != 1.0) {
+  //     if (navigator.userAgent.indexOf('Firefox') != -1) elm.style.boxShadow = 'none';  // масштабирование в Firefox порождало некорректное отображение boxshadow, и пришлось это свойство отключить
+  //     elm.style.webkitTransform = 'scale(' + coeff + ')';
+  //     elm.style.msTransform =
+  //       elm.style.mozTransform =
+  //         elm.style.transform = 'scale(' + coeff + ')'; // собственно масштабирование
+  //   }
+  // };
+  // window.onresize = function (event) {
+  //   var elm = document.getElementById('main-frame'); // all -- элемент, в который был обернут весь сайт
+  //   var coeff = document.documentElement.clientHeight / elm.offsetHeight; // считаем коэффициент масштабирования так, чтобы элемент all занял весь экран
+  //   if (coeff > 1) coeff = 1; // нам нужно только уменьшение сайта, но не его увеличение, поэтому ограничиваем коэффициент сверху единицей
+  //   if (coeff < 0.6) coeff = 0.6; // ограничение снизу добавлено для того, чтобы сайт совсем уж не превращался в нечитаемый
+  //   if (coeff != 1.0) {
+  //     if (navigator.userAgent.indexOf('Firefox') != -1) elm.style.boxShadow = 'none';  // масштабирование в Firefox порождало некорректное отображение boxshadow, и пришлось это свойство отключить
+  //     elm.style.webkitTransform = 'scale(' + coeff + ')';
+  //     elm.style.msTransform =
+  //       elm.style.mozTransform =
+  //         elm.style.transform = 'scale(' + coeff + ')'; // собственно масштабирование
+  //   }
+  //
+  // };
 
 
   //console.log('isDevice===', isDevice);
