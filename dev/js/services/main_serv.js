dd080fc616c2ee4f1b3826b4ae794c9b4ac63b5d
Yaroslav <bublik@splinestudio.com>
2017-01-17 18:23:02 +0200

    fixed saving template_source if is_addElem_only === 1
    Former-commit-id: 9d940226377dd24ec4be67eedc68151c6d9ff28a

--- dev/js/services/main_serv.js
+++ dev/js/services/main_serv.js
@@ -1593,7 +1593,7 @@
             }
           }
           productData.lamination_id = OrderStor.order.products[p].lamination.id;
-          productData.template_source = JSON.stringify(productData.template_source);
+          productData.template_source = (!productData.is_addelem_only) ? JSON.stringify(productData.template_source): JSON.stringify({});
           productData.lamination_in_id = OrderStor.order.products[p].lamination.img_in_id;
           productData.lamination_out_id = OrderStor.order.products[p].lamination.img_out_id;
           productData.modified = new Date();
@@ -1671,7 +1671,7 @@
                 };
 
 
-                console.log('SEND ADD',addElementsData);
+                //console.log('SEND ADD',addElementsData);
                 if(orderType) {
                   localDB.insertRowLocalDB(addElementsData, localDB.tablesLocalDB.order_addelements.tableName);
                   localDB.insertServer(
