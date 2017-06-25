643ffe62c6bb04d6643a9f544a556e97fac0af8e
anton <a.shevelev@fpmsoft.com>
2017-06-25 10:19:17 +0300

    exception added
    Former-commit-id: 15072649493cfb7d9d59e3660fd42914d9c2f431

--- dev/js/services/general_serv.js
+++ dev/js/services/general_serv.js
@@ -218,27 +218,27 @@
 
 
         $window.onbeforeunload = function () {
-          var order = localStorage.getItem("OrderStor");
-          var product = localStorage.getItem("ProductStor");
-          var aux = localStorage.getItem("AuxStor");
-          var design = localStorage.getItem("DesignStor");
-          var user = localStorage.getItem("UserStor");
-          var global = localStorage.getItem("GlobalStor");
-          if (product && user && global && design && order && aux) {
-            var global = LZString.compress(JSON.stringify(GlobalStor.global));
-            var product = LZString.compress(JSON.stringify(ProductStor.product));
-            var userInfo = LZString.compress(JSON.stringify(UserStor.userInfo));
-            var design = LZString.compress(JSON.stringify(DesignStor.design));
-            var aux = LZString.compress(JSON.stringify(AuxStor.aux));
-            var order = LZString.compress(JSON.stringify(OrderStor.order));
-
-            localStorage.setItem('GlobalStor', global);
-            localStorage.setItem('ProductStor', product);
-            localStorage.setItem('UserStor', userInfo);
-            localStorage.setItem('AuxStor', aux);
-            localStorage.setItem('DesignStor', design);
-            localStorage.setItem('OrderStor', order);
-          }
+          // var order = localStorage.getItem("OrderStor");
+          // var product = localStorage.getItem("ProductStor");
+          // var aux = localStorage.getItem("AuxStor");
+          // var design = localStorage.getItem("DesignStor");
+          // var user = localStorage.getItem("UserStor");
+          // var global = localStorage.getItem("GlobalStor");
+          // if (product && user && global && design && order && aux) {
+          //   var global = LZString.compress(JSON.stringify(GlobalStor.global));
+          //   var product = LZString.compress(JSON.stringify(ProductStor.product));
+          //   var userInfo = LZString.compress(JSON.stringify(UserStor.userInfo));
+          //   var design = LZString.compress(JSON.stringify(DesignStor.design));
+          //   var aux = LZString.compress(JSON.stringify(AuxStor.aux));
+          //   var order = LZString.compress(JSON.stringify(OrderStor.order));
+          //
+          //   localStorage.setItem('GlobalStor', global);
+          //   localStorage.setItem('ProductStor', product);
+          //   localStorage.setItem('UserStor', userInfo);
+          //   localStorage.setItem('AuxStor', aux);
+          //   localStorage.setItem('DesignStor', design);
+          //   localStorage.setItem('OrderStor', order);
+          // }
           // return $filter('translate')('common_words.PAGE_REFRESH');
         };
 
