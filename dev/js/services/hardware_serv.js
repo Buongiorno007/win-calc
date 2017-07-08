a8c52a08d67b747a756c7b949b17c332f37fba33
anton <a.shevelev@fpmsoft.com>
2017-07-09 01:39:30 +0300

    light v powerfull upgrade
    Former-commit-id: fba3276642da817c81af2309fe1da4c3d7227e5b

--- dev/js/services/hardware_serv.js
+++ dev/js/services/hardware_serv.js
@@ -1,76 +1,75 @@
-(function(){
+(function () {
   'use strict';
   /**@ngInject*/
   angular
     .module('HistoryModule')
     .factory('HardwareServ',
 
-  function(
-    $location,
-    $filter,
-    $q,
-    ProductStor,
-    MainServ,
-    DesignStor,
-    AnalyticsServ,
-    UserStor,
-    OrderStor,
-    GlobalStor
-
-  ) {
-    /*jshint validthis:true */
-    var thisFactory = this;
+      function ($location,
+                $filter,
+                $q,
+                ProductStor,
+                MainServ,
+                DesignStor,
+                AnalyticsServ,
+                UserStor,
+                OrderStor,
+                GlobalStor) {
+        /*jshint validthis:true */
+        var thisFactory = this;
 
 
-    /**============ METHODS ================*/
-    
-    /**----------- Select hardware -------- */
-    function selectHardware(newId) {
-      GlobalStor.global.isChangedTemplate = 1;
-      if(ProductStor.product.hardware.id !== newId) {
+        /**============ METHODS ================*/
 
-        /** check sizes of all hardware in sashes */
-        MainServ.checkHardwareSizes(ProductStor.product.template, newId);
+        /**----------- Select hardware -------- */
+        function selectHardware(newId) {
+          GlobalStor.global.isChangedTemplate = 1;
+          if (ProductStor.product.hardware.id !== newId) {
 
-        if(DesignStor.design.extraHardware.length){
-          /** there are incorrect sashes
-           * expose Alert */
-          DesignStor.design.isHardwareExtra = 1;
-        } else {
-          //-------- set current Hardware
-          MainServ.setCurrentHardware(ProductStor.product, newId);
-          //------ calculate price
-          MainServ.preparePrice(
-            ProductStor.product.template,
-            ProductStor.product.profile.id,
-            ProductStor.product.glass,
-            ProductStor.product.hardware.id,
-            ProductStor.product.lamination.lamination_in_id
-          );
-          //------ save analytics data
+            /** check sizes of all hardware in sashes */
+            MainServ.checkHardwareSizes(ProductStor.product.template, newId);
+
+            if (DesignStor.design.extraHardware.length) {
+              /** there are incorrect sashes
+               * expose Alert */
+              DesignStor.design.isHardwareExtra = 1;
+            } else {
+              //-------- set current Hardware
+              MainServ.setCurrentHardware(ProductStor.product, newId);
+              if ($location.path() !== "/light") {
+                //------ calculate price
+                MainServ.preparePrice(
+                  ProductStor.product.template,
+                  ProductStor.product.profile.id,
+                  ProductStor.product.glass,
+                  ProductStor.product.hardware.id,
+                  ProductStor.product.lamination.lamination_in_id
+                );
+              }
+              //------ save analytics data
 //AnalyticsServ.saveAnalyticDB(UserStor.userInfo.id, OrderStor.order.id, ProductStor.product.template_id, newId, 3);
-          /** send analytics data to Server*/
-          AnalyticsServ.sendAnalyticsData(
-            UserStor.userInfo.id,
-            OrderStor.order.id,
-            ProductStor.product.template_id,
-            newId,
-            3
-          );
+              /** send analytics data to Server*/
+              AnalyticsServ.sendAnalyticsData(
+                UserStor.userInfo.id,
+                OrderStor.order.id,
+                ProductStor.product.template_id,
+                newId,
+                3
+              );
+            }
+          }
         }
-      }
-    }
-    /**========== FINISH ==========*/
-    //------ clicking
-    selectHardware:selectHardware;
 
-    thisFactory.publicObj = {
-      selectHardware:selectHardware
-    };
+        /**========== FINISH ==========*/
+        //------ clicking
+        selectHardware:selectHardware;
 
-    return thisFactory.publicObj;
+        thisFactory.publicObj = {
+          selectHardware: selectHardware
+        };
 
+        return thisFactory.publicObj;
 
 
-  });
+      });
 })();
