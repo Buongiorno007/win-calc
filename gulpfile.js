6387c37f4ceccf1187543ac78801b393b3ec5fe1
anton <a.shevelev@fpmsoft.com>
2017-07-17 12:04:39 +0300

    test
    Former-commit-id: 556ff6dc09bb572d0e469d347d4d9f1707e98b5d

--- gulpfile.js
+++ gulpfile.js
@@ -49,7 +49,8 @@ var server_env = {
     "window": "'http://windowscalculator.net/orders/get-order-pdf/'"
   },
   path_env = {
-    "windowSiteTest": "'/calculator/local/'",
+    // "windowSiteTest": "'/calculator/local/'",
+    "windowSiteTest": "'/local/'",
     "windowSite": "'/calculator/local/'",
     "steko": "'/local/'",
     "orange": "'/local/'",
