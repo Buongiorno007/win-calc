464260cee1763980c7b6b011523e10b0e51ceecc
Anthony <a.shevelev@fpmsoft.com>
2018-04-11 17:05:39 +0300

    test
    Former-commit-id: f06a196dc202b8e235faf2e6414d2fa5a6c6bd38

--- gulpfile.js
+++ gulpfile.js
@@ -438,16 +438,12 @@ function buildSite(id) {
         .pipe(replace('LOCAL_PATH', path_env[id]))
         .pipe(replace('ISEXTFLAG', "0"))
         .pipe(concat('main.js'))
-        .pipe(removeLogs())
-        .pipe(js_obfuscator())
-        .pipe(babel({
-            presets: ['env']
-        }))
-        .pipe(ngAnnotate({
-            remove: true,
-            add: true,
-            single_quotes: true
-        }))
+        // .pipe(removeLogs())
+        // .pipe(js_obfuscator())
+        // .pipe(babel({
+        //     presets: ['env']
+        // }))
+        .pipe(ngAnnotate())
         .pipe(gulp.dest("_product/" + id + "/site/js"))
         .on('end', function () {
             gutil.log('js!');
