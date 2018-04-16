6cd312a8c842f73373e7390a412ddffccffdf263
Antonio <a.shevelev@fpmsoft.com>
2018-04-16 15:33:28 +0300

    temp save
    Former-commit-id: 9f21383ccf788a17ec26857a7c4898d94e85ea88

--- dev/js/controllers/panels/glasses.js
+++ dev/js/controllers/panels/glasses.js
@@ -7,6 +7,7 @@
 
       function($filter,
         $location,
+        $timeout,
         $anchorScroll,
         globalConstants,
         MainServ,
@@ -153,24 +154,25 @@
             });
         }
 
-        function OpenGlassFolder(index, event) {
+        function OpenGlassFolder(index) {
           if (thisCtrl.OpenFolder === index) {
             thisCtrl.OpenFolder = -1;
           } else {
             thisCtrl.OpenFolder = index;
             if (event) {
-              if ($location.path() === "/mobile") {
-                setTimeout(() => {
-                  $('.glass-container').animate({
-                    scrollTop: $(event.currentTarget).offset().top + $('.glass-container').scrollTop() - $(event.currentTarget).height() / 2 - $('.accept-container').height()
-                  }, 'slow');
-                }, 250);
-              } else {
-                event.preventDefault();
-                setTimeout(() => {
-                  $anchorScroll('anchor' + index);
-                }, 50);
-              }
+                if ($location.path() === "/mobile") {
+                  $timeout(()=>{
+                    $anchorScroll('anchor' + index);
+                    $timeout(()=>{
+                      $('.glass-container').animate({ scrollTop: $('.glass-container').scrollTop() - 20 }, 'slow');
+                    },100);
+                  },100);
+                } else {
+                  $timeout(()=>{
+                    $anchorScroll('anchor' + index);
+                  },100);
+                }
+
             }
           }
         }
