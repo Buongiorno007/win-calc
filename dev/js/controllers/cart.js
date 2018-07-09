94a830b024843c3eeebf275a2287b5f367e98bf5
AntonIO <a.shevelev@fpmsoft.com>
2018-07-09 20:41:12 +0300

    recalculate price in cart
    Former-commit-id: 0e6e9cc59c2973a61bd25a3a239e445972834b86

--- dev/js/controllers/cart.js
+++ dev/js/controllers/cart.js
@@ -115,6 +115,8 @@
 
                 CartMenuServ.calculateAverageDisc();
 
+                CartMenuServ.swipeDiscountBlock(true)
+
                 // console.log('cart +++++', JSON.stringify(OrderStor.order));
                 //-------- return from Main Page
                 if (GlobalStor.global.prevOpenPage === 'main') {
